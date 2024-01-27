6bd9b57c213e05b962222d02b29be3bf7861c33d
Aram A. Ahmad <aramaahmad42@gmail.com>
2024-01-27 16:30:40 +0300

    Install Meterials

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "./index.css";
+import { IoIosChatbubbles } from "react-icons/io";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-6xl text-red-600">Hello world</h1>
+      <IoIosChatbubbles />
     </div>
   );
 }
